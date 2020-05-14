30741d5b60f2f2ad837a3cd1885844b885706458
yippptay <yippptay@gmail.com>
2020-05-14 05:26:22 -0700

    Finished.

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 // all non-status moves have an acc of 70% and pwr of 40
-let pkmnList = ["Treecko", "Mudkip", "Torchic"];
-var userHP = 100;
-var opHP = 100;
+
+/* playerPkmn */
+let playerPkmn;
+
+/* opponentPkmn */
+let opponentPkmn;
+
+/* stats */
 playerMove = 0;
 
 /* default constructor */
@@ -60,7 +65,121 @@ class pkmn {
   }
 
   /* treecko's moves */
+  pound(pkmn) {
+    /* pokemon's damage formula */
+    let dmg =
+      (((2 * this.lvl) / 5 + 2) * 40 * (this.att / pkmn.getDef())) / 50 + 2;
+    let chance = Math.random() * 10;
 
+    if (chance < 7) {
+      // default ACC
+      let remainingHp = pkmn.getHp() - Math.round(dmg);
+      pkmn.setHp(remainingHp);
+      $(".message").text("Treecko used POUND!");
+    } else {
+      $(".message").text("Treecko missed!");
+    }
+  }
+
+  leer(pkmn) {
+    let remainingDef = pkmn.getDef() - 3;
+    $(".message").text("Treecko used LEER! Enemy defense lowered!");
+  }
+
+  absorb(pkmn) {
+    /* pokemon's damage formula */
+    let dmg =
+      (((2 * this.lvl) / 5 + 2) * 40 * (this.att / pkmn.getDef())) / 50 + 2;
+    let chance = Math.random() * 10;
+
+    if (chance < 7) {
+      // default ACC
+      let remainingHp = pkmn.getHp() - Math.round(dmg);
+      pkmn.setHp(remainingHp);
+      $(".message").text(
+        "Treecko used ABSORB! Treecko gained " + Math.round(dmg / 2) + " HP."
+      );
+      this.hp = this.hp + Math.round(dmg / 2);
+    } else {
+      $(".message").text("Treecko missed!");
+    }
+  }
+
+  /* torchic's moves */
+  scratch(pkmn) {
+    /* pokemon's damage formula */
+    let dmg =
+      (((2 * this.lvl) / 5 + 2) * 40 * (this.att / pkmn.getDef())) / 50 + 2;
+    let chance = Math.random() * 10;
+
+    if (chance < 7) {
+      // default ACC
+      let remainingHp = pkmn.getHp() - Math.round(dmg);
+      pkmn.setHp(remainingHp);
+      $(".message").text("Torchic used SCRATCH!");
+    } else {
+      $(".message").text("Torchic missed!");
+    }
+  }
+
+  torGrowl(pkmn) {
+    let remainingDef = pkmn.getAtt() - 3;
+    $(".message").text("Torchic used GROWL! Enemy attack lowered!");
+  }
+
+  ember(pkmn) {
+    /* pokemon's damage formula */
+    let dmg =
+      (((2 * this.lvl) / 5 + 2) * 40 * (this.att / pkmn.getDef())) / 50 + 2;
+    let chance = Math.random() * 10;
+
+    if (chance < 7) {
+      // default ACC
+      let remainingHp = pkmn.getHp() - Math.round(dmg);
+      pkmn.setHp(remainingHp);
+      $(".message").text("Torchic used EMBER!");
+    } else {
+      $(".message").text("Torchic missed!");
+    }
+  }
+
+  /* mudkip's moves */
+  tackle(pkmn) {
+    /* pokemon's damage formula */
+    let dmg =
+      (((2 * this.lvl) / 5 + 2) * 40 * (this.att / pkmn.getDef())) / 50 + 2;
+    let chance = Math.random() * 10;
+
+    if (chance < 7) {
+      // default ACC
+      let remainingHp = pkmn.getHp() - Math.round(dmg);
+      pkmn.setHp(remainingHp);
+      $(".message").text("Mudkip used TACKLE!");
+    } else {
+      $(".message").text("Mudkip missed!");
+    }
+  }
+
+  mudGrowl(pkmn) {
+    let remainingDef = pkmn.getAtt() - 3;
+    $(".message").text("Mudkip used GROWL! Enemy attack lowered!");
+  }
+
+  watergun(pkmn) {
+    /* pokemon's damage formula */
+    let dmg =
+      (((2 * this.lvl) / 5 + 2) * 40 * (this.att / pkmn.getDef())) / 50 + 2;
+    let chance = Math.random() * 10;
+
+    if (chance < 7) {
+      // default ACC
+      let remainingHp = pkmn.getHp() - Math.round(dmg);
+      pkmn.setHp(remainingHp);
+      $(".message").text("Mudkip used WATERGUN!");
+    } else {
+      $(".message").text("Mudkip missed!");
+    }
+  }
 }
 
 /* pkmn list */
@@ -68,226 +187,258 @@ const mudkip = new pkmn(5, 50, 70, 50, 50, 50, 40);
 const treecko = new pkmn(5, 40, 45, 35, 65, 55, 70);
 const torchic = new pkmn(5, 45, 60, 40, 70, 50, 45);
 
+/* opponent generator */
+let pkmnList = [mudkip, treecko, torchic];
+let randomPoke = Math.round(Math.random() * 2);
+opponentPkmn = pkmnList[randomPoke];
+
 /* stage builder */
-$(function() {
-  $(".treecko").click(function(){
+$(function () {
+  $(".treecko").click(function () {
+    /* hide intro and start game */
     $(".intro").hide();
     $(".box").show();
     $(".game").show();
 
-    $(".playerPokemon").attr("src", "https://img.pokemondb.net/sprites/black-white/anim/back-normal/treecko.gif")
+    /* opponent generator */
+    if (randomPoke == 0) {
+      $(".opponentPokemon").attr(
+        "src",
+        "https://img.pokemondb.net/sprites/black-white/anim/normal/mudkip.gif"
+      );
+      $(".opponentName").text("Mudkip");
+    } else if (randomPoke == 1) {
+      $(".opponentPokemon").attr(
+        "src",
+        "https://img.pokemondb.net/sprites/black-white/anim/normal/treecko.gif"
+      );
+      $(".opponentName").text("Treecko");
+    } else {
+      $(".opponentPokemon").attr(
+        "src",
+        "https://img.pokemondb.net/sprites/black-white/anim/normal/torchic.gif"
+      );
+      $(".opponentName").text("Torchic");
+    }
 
-    $(".playerName").text("Treecko")
+    /* update js */
+    playerPkmn = treecko;
 
-    $(".message").text("What should TREECKO do?")
-  })
+    /* update player ui*/
+    $(".playerPokemon").attr(
+      "src",
+      "https://img.pokemondb.net/sprites/black-white/anim/back-normal/treecko.gif"
+    );
+    $(".playerName").text("Treecko");
+    $(".message").text("What should TREECKO do?");
+    $(".move1").text("Pound");
+    $(".move2").text("Leer");
+    $(".move3").text("Absorb");
+    $(".playerHp-count").text(playerPkmn.getHp());
 
-  $(".mudkip").click(function(){
+    /* update opponent ui*/
+    $(".opponentHp-count").text(opponentPkmn.getHp());
+  });
+
+  $(".mudkip").click(function () {
+    /* hide intro and start game */
     $(".intro").hide();
     $(".box").show();
     $(".game").show();
 
-    $(".playerPokemon").attr("src", "https://img.pokemondb.net/sprites/black-white/anim/back-normal/mudkip.gif")
+    /* opponent generator */
+    if (randomPoke == 0) {
+      $(".opponentPokemon").attr(
+        "src",
+        "https://img.pokemondb.net/sprites/black-white/anim/normal/mudkip.gif"
+      );
+      $(".opponentName").text("Mudkip");
+    } else if (randomPoke == 1) {
+      $(".opponentPokemon").attr(
+        "src",
+        "https://img.pokemondb.net/sprites/black-white/anim/normal/treecko.gif"
+      );
+      $(".opponentName").text("Treecko");
+    } else {
+      $(".opponentPokemon").attr(
+        "src",
+        "https://img.pokemondb.net/sprites/black-white/anim/normal/torchic.gif"
+      );
+      $(".opponentName").text("Torchic");
+    }
 
-    $(".playerName").text("Mudkip")
+    /* update js */
+    playerPkmn = mudkip;
 
-    $(".message").text("What should MUDKIP do?")
-  })
+    /* update player ui*/
+    $(".playerPokemon").attr(
+      "src",
+      "https://img.pokemondb.net/sprites/black-white/anim/back-normal/mudkip.gif"
+    );
+    $(".playerName").text("Mudkip");
+    $(".message").text("What should MUDKIP do?");
+    $(".move1").text("Tackle");
+    $(".move2").text("Growl");
+    $(".move3").text("Water Gun");
+    $(".playerHp-count").text(playerPkmn.getHp());
 
-  $(".torchic").click(function(){
+    /* update opponent ui*/
+    $(".opponentHp-count").text(opponentPkmn.getHp());
+  });
+
+  $(".torchic").click(function () {
+    /* hide intro and start game */
     $(".intro").hide();
     $(".box").show();
     $(".game").show();
 
-    $(".playerPokemon").attr("src", "https://img.pokemondb.net/sprites/black-white/anim/back-normal/torchic.gif")
-
-    $(".playerName").text("Torchic")
-
-    $(".message").text("What should TORCHIC do?")
-  })
-})
-
-/* moveHierarchy */
-
-
-/* mudkip's moves */
-function waterGun() {
-  if (playerMove == 0 && userHP != 0) {
-    var miss = Math.floor(Math.random() * 10 + 1); // miss rate
-    if (miss == 1) {
-      document.getElementById("message").innerHTML =
-        " Blastoises attack missed! ";
+    /* opponent generator */
+    if (randomPoke == 0) {
+      $(".opponentPokemon").attr(
+        "src",
+        "https://img.pokemondb.net/sprites/black-white/anim/normal/mudkip.gif"
+      );
+      $(".opponentName").text("Mudkip");
+    } else if (randomPoke == 1) {
+      $(".opponentPokemon").attr(
+        "src",
+        "https://img.pokemondb.net/sprites/black-white/anim/normal/treecko.gif"
+      );
+      $(".opponentName").text("Treecko");
     } else {
-      document.getElementById("message").innerHTML =
-        " Blastoise used water cannon!"; // attack
-      var critical = Math.floor(Math.random() * 10 + 1); // critical
-      if (critical == 4) {
-        for (var x = 0; x < 2; x++) {
-          opHP = opHP - 30; // yes critical
-        }
-      } else {
-        opHP = opHP - 30; // no critical
-      }
-      if (opHP < 0) {
-        opHP = 0;
-      } //faint
-      document.getElementById("apHP").innerHTML = opHP; // update hp
-      if (opHP == 0) {
-        document.getElementById("message").innerHTML = " Charizard fainted!"; // update message
-      }
+      $(".opponentPokemon").attr(
+        "src",
+        "https://img.pokemondb.net/sprites/black-white/anim/normal/torchic.gif"
+      );
+      $(".opponentName").text("Torchic");
     }
-    //wait();
-    playerMove = 1; // update player move
+
+    /* update js */
+    playerPkmn = torchic;
+
+    /* update player ui*/
+    $(".playerPokemon").attr(
+      "src",
+      "https://img.pokemondb.net/sprites/black-white/anim/back-normal/torchic.gif"
+    );
+    $(".playerName").text("Torchic");
+    $(".message").text("What should TORCHIC do?");
+    $(".move1").text("Scratch");
+    $(".move2").text("Growl");
+    $(".move3").text("Ember");
+    $(".playerHp-count").text(playerPkmn.getHp());
+
+    /* update opponent ui*/
+    $(".opponentHp-count").text(opponentPkmn.getHp());
+  });
+});
+
+/* stats */
+var userHP = playerPkmn.getHp();
+var opHP = opponentPkmn.getHp();
+
+/* moveCreator */
+// player
+function pMove1() {
+  if (playerPkmn == torchic) {
+    playerPkmn.scratch(opponentPkmn);
   }
-}
-function growl() {
-  if (playerMove == 0 && userHP != 0) {
-    var miss = Math.floor(Math.random() * 10 + 1);
-    if (miss == 1) {
-      document.getElementById("message").innerHTML =
-        " Blastoise's attack missed!";
-    } else {
-      document.getElementById("message").innerHTML =
-        " Blastoise used water pulse!";
-      var critical = Math.floor(Math.random() * 10 + 1);
-      if (critical == 4) {
-        for (var x = 0; x < 2; x++) {
-          opHP = opHP - 20;
-        }
-      } else {
-        opHP = opHP - 20;
-      }
-      if (opHP < 0) {
-        opHP = 0;
-      }
-      document.getElementById("apHP").innerHTML = opHP;
-      //document.getElementById("message").innerHTML = " Charizard2 "
-      if (opHP == 0) {
-        document.getElementById("message").innerHTML = " Charizard fainted! ";
-      }
-    }
-    //wait();
-    playerMove = 1;
+  if (playerPkmn == mudkip) {
+    playerPkmn.tackle(opponentPkmn);
   }
-}
-function tackle() {
-  if (playerMove == 0 && userHP != 0) { // initial check
-    
-    var miss = Math.floor(Math.random() * 10 + 1); 
-    if (miss == 1) {
-      document.getElementById("message").innerHTML =
-        " Mudkip's attack missed!";
-    } else {
-      document.getElementById("message").innerHTML = " Blastoise used tackle!";
-      var critical = Math.floor(Math.random() * 10 + 1);
-      if (critical == 4) {
-        for (var x = 0; x < 2; x++) {
-          opHP = opHP - 5;
-        }
-      } else {
-        opHP = opHP - 5;
-      }
-      if (opHP < 0) {
-        opHP = 0;
-      }
-      document.getElementById("apHP").innerHTML = opHP;
-      //document.getElementById("message").innerHTML = " Charizard4 "
-      if (opHP == 0) {
-        document.getElementById("message").innerHTML = " Charizard fainted!";
-      }
-    }
-    //wait();
-    playerMove = 1;
+  if (playerPkmn == treecko) {
+    playerPkmn.pound(opponentPkmn);
   }
+  $(".opponentHp-count").text(opponentPkmn.getHp());
+  $(".playerHp-count").text(playerPkmn.getHp());
 }
 
-/* torhcic's moves */
-function scratch() {
-  var miss = Math.floor(Math.random() * 10 + 1); // miss rate
-  if (miss == 1) {
-    document.getElementById("message").innerHTML =
-      " Charizard's attack missed!"; // attack missed
-  } else {
-    document.getElementById("message").innerHTML =
-      " Charizard used flame thrower!"; // attack
-    var critical = Math.floor(Math.random() * 10 + 1); // critical
-    if (critical == 4) {
-      for (var x = 0; x < 2; x++) {
-        // yes critical
-        userHP = userHP - 30;
-      }
-    } else {
-      userHP = userHP - 30; // no critical
-    }
-    if (userHP < 0) {
-      userHP = 0;
-    } // faint
-    document.getElementById("myHP").innerHTML = userHP; // update hp
-    if (userHP == 0) {
-      // fainted
-      document.getElementById("message").innerHTML = " Blastoise fainted!"; // fainted
-    }
+function pMove2() {
+  if (playerPkmn == torchic) {
+    playerPkmn.torGrowl(opponentPkmn);
   }
-}
-function growl() {
-  var miss = Math.floor(Math.random() * 10 + 1);
-  if (miss == 1) {
-    document.getElementById("message").innerHTML =
-      " Charizard's attack missed!";
-  } else {
-    document.getElementById("message").innerHTML =
-      " Charizard used dragon claw!";
-    var critical = Math.floor(Math.random() * 10 + 1);
-    if (critical == 4) {
-      for (var x = 0; x < 2; x++) {
-        userHP = userHP - 20;
-      }
-    } else {
-      userHP = userHP - 20;
-    }
-    if (userHP < 0) {
-      userHP = 0;
-    }
-    document.getElementById("myHP").innerHTML = userHP;
-    if (userHP == 0) {
-      document.getElementById("message").innerHTML = " Blastoise fainted!";
-    }
+  if (playerPkmn == mudkip) {
+    playerPkmn.mudGrowl(opponentPkmn);
   }
-}
-function ember() {
-  var miss = Math.floor(Math.random() * 10 + 1);
-  if (miss == 1) {
-    document.getElementById("message").innerHTML =
-      " Charizard's attack missed!";
-  } else {
-    document.getElementById("message").innerHTML = " Charizard used ember!";
-    var critical = Math.floor(Math.random() * 10 + 1);
-    if (critical == 4) {
-      for (var x = 0; x < 2; x++) {
-        userHP = userHP - 10;
-      }
-    } else {
-      userHP = userHP - 10;
-    }
-    if (userHP < 0) {
-      userHP = 0;
-    }
-    document.getElementById("myHP").innerHTML = userHP;
-    if (userHP == 0) {
-      document.getElementById("message").innerHTML = " Blastoise fainted!";
-    }
+  if (playerPkmn == treecko) {
+    playerPkmn.leer(opponentPkmn);
   }
+  $(".opponentHp-count").text(opponentPkmn.getHp());
+  $(".playerHp-count").text(playerPkmn.getHp());
 }
 
-/* treecko's moves */
+function pMove3() {
+  if (playerPkmn == torchic) {
+    playerPkmn.ember(opponentPkmn);
+  }
+  if (playerPkmn == mudkip) {
+    playerPkmn.watergun(opponentPkmn);
+  }
+  if (playerPkmn == treecko) {
+    playerPkmn.absorb(opponentPkmn);
+  }
+  $(".opponentHp-count").text(opponentPkmn.getHp());
+  $(".playerHp-count").text(playerPkmn.getHp());
+}
 
-/* turn */
-// function compPokemon() {
-//   // continue
-//   if (playerMove == 1 && opHP != 0) {
-//     // whos move
-//     var move = Math.floor(Math.random() * 4 + 1); // choose move randomly
-//     opAttacks[move](); // call attack from array
-//     playerMove = 0; // update player move
-//   }
-// }
+// enemy
+function enemyMove() {
+  if (opponentPkmn == torchic) {
+    let randMove = Math.round(Math.random() * 2);
+    if (randMove == 0) {
+      opponentPkmn.scratch(playerPkmn);
+    } else if (randMove == 1) {
+      opponentPkmn.torGrowl(playerPkmn);
+    } else {
+      opponentPkmn.ember(playerPkmn);
+    }
+  }
+  if (opponentPkmn == mudkip) {
+    let randMove = Math.round(Math.random() * 2);
+    if (randMove == 0) {
+      opponentPkmn.tackle(playerPkmn);
+    } else if (randMove == 1) {
+      opponentPkmn.mudGrowl(playerPkmn);
+    } else {
+      opponentPkmn.watergun(playerPkmn);
+    }
+  }
+  if (opponentPkmn == treecko) {
+    let randMove = Math.round(Math.random() * 2);
+    if (randMove == 0) {
+      opponentPkmn.pound(playerPkmn);
+    } else if (randMove == 1) {
+      opponentPkmn.leer(playerPkmn);
+    } else {
+      opponentPkmn.absorb(playerPkmn);
+    }
+  }
+  $(".opponentHp-count").text(opponentPkmn.getHp());
+  $(".playerHp-count").text(playerPkmn.getHp());
+}
+
+/* end of round */
+function endRound() {
+  $(".opponentHp-count").text(opponentPkmn.getHp());
+  $(".playerHp-count").text(playerPkmn.getHp());
+  if (playerPkmn == torchic) {
+    $(".message").text("What should TORCHIC do?");
+  }
+  if (playerPkmn == mudkip) {
+    $(".message").text("What should MUDKIP do?");
+  }
+  if (playerPkmn == treecko) {
+    $(".message").text("What should TREECKO do?");
+  }
+  if (playerPkmn.getHp() < 0) {
+    $(".game").hide();
+    $(".box").hide();
+    $(".ending").show();
+    $(".endingtextbox").text("Your pokemon fainted! Game over.");
+  } else if (opponentPkmn.getHp() < 0) {
+    $(".game").hide();
+    $(".box").hide();
+    $(".ending").show();
+    $(".endingtextbox").text("Enemy pokemon fainted! You win!");
+  }
+}
